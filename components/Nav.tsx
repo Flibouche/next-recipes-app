43b7b6f6a0fa2338f1ebdf5162d0e48c0f462fe8
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-05 16:05:21 +0100

    📦 NEW: add links to the navbar.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
+"use client";
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const links = [
+    {
+        name: 'Home',
+        path: '/',
+    },
+    {
+        name: 'recipes',
+        path: '/recipe',
+    },
+    {
+        name: 'ingredients',
+        path: '/ingredient',
+    },
+    {
+        name: 'blog',
+        path: '/blog',
+    },
+];
 
 const Nav = () => {
+    const pathname = usePathname();
     return (
-        <nav>
-            <ul className='flex gap-x-5'>
-                <li>Home</li>
-                <li>Recipes</li>
-                <li>Ingredients</li>
-            </ul>
+        <nav className='flex gap-8'>
+            {links.map((link, index) => {
+                return (
+                    <Link
+                        href={link.path}
+                        key={index}
+                        className={`${link.path === pathname && "text-blue-500 border-b-2 border-accent"
+                            } capitalize `}
+                    >
+                        {link.name}
+                    </Link>
+                );
+            })}
         </nav>
     )
 }
