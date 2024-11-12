2ea697f402c2c6ac67b6253eb0d9e6985eea7027
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-12 23:27:20 +0100

    👌 IMPROVE: add fetchRecipes to the recipe page.

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,12 +1,47 @@
+"use client";
+
 import Link from 'next/link'
+import { useEffect, useState } from 'react';
 
 const Recipe = () => {
+    const [recipes, setRecipes] = useState([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                const response = await fetch('/api/recipe');
+                if (!response.ok) {
+                    setError('Failed to fetch recipes');
+                }
+
+                const data = await response.json();
+
+                setRecipes(data);
+            } catch (error) {
+                setError(error instanceof Error ? error.message : 'An error occurred');
+            }
+        }
+        fetchRecipes();
+    }, []);
+
+    if (error) {
+        throw new Error(error);
+    }
+
     return (
         <>
             <h1>Ingredients :</h1>
             <nav>
                 <Link href='/recipe/add'>Add a recipe</Link>
             </nav>
+            <div>
+                {recipes.map((recipe: any) => (
+                    <div key={recipe.id}>
+                        <h2>{recipe.name}</h2>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
