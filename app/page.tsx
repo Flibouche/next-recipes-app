bf42622b797f93a3a8d8c48ba9a54de0ab26568a
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-13 16:00:59 +0100

    📦 NEW: add admin, sign-in, sign-up page; add userUtils and config the middleware correctly.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import Image from "next/image";
+import { checkUserRole } from "../utils/userUtils";
+import { useSession } from "@clerk/nextjs";
 
 export default function Home() {
+    const { session } = useSession();
+    const userRole = checkUserRole(session);
+
     return (
         <section>
             <h1>Home</h1>
+            {session ? (
+                <p>Welcome, you are connected !</p>
+            ) : (
+                <p>Welcome, you are not connected !</p>
+            )}
         </section>
     );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,15 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+// export default clerkMiddleware(async (auth, request) => {
+//     const authData = await auth()
+
+//     // Check if the route is private and the user is not authenticated
+//     if (!isPublicRoute(request) && !authData.userId) {
+//         return new Response("Unauthorized", { status: 401 })
+//     }
+// })
 
 export default clerkMiddleware();
 
@@ -9,4 +20,4 @@ export const config = {
         // Always run for API routes
         '/(api|trpc)(.*)',
     ],
-};
+}
