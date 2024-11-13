305413b1bb0e82143836b770e3d8a3026584c738
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-13 15:13:31 +0100

    📦 NEW: add middleware and clerkprovider to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 // Components
 import Header from "@/components/Header";
@@ -12,14 +13,24 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <html lang="en">
-            <body>
-                <Header />
-                <main className="container">
-                    {children}
-                </main>
-                <Footer />
-            </body>
-        </html>
+        <ClerkProvider>
+            <html lang="en">
+                <body>
+                    <Header />
+                    <div>
+                        <SignedOut>
+                            <SignInButton />
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </div>
+                    <main className="container">
+                        {children}
+                    </main>
+                    <Footer />
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
