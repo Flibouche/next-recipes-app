5a9e0e72bffc516deae1954058084e619f2b09d7
Flibouche <kevin.pfiffer2@gmail.com>
2024-11-24 00:23:46 +0100

    📦 NEW: Add better metadata to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,16 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-    title: "Next Recipes App",
+    title: "Your Next Recipe",
     description: "A recipes app built with Next.js",
+    openGraph: {
+        title: "Your Next Recipe",
+        description: "A cooking recipes app built with Next.js",
+        url: "https://your-next-recipe.vercel.app",
+        siteName: "Your Next Recipe",
+        locale: "en_US",
+        type: "website",
+    }
 };
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
