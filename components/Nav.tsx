f65cabd4644ba74b070afbc22450ed3e38c20b6d
Flibouche <kevin.pfiffer2@gmail.com>
2024-12-01 00:14:42 +0100

    📦 NEW: Add dropdown menu to Nav component to make some tests.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,6 +4,9 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import ThemeToggle from './ThemeToggle';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { IoChevronDownCircleOutline } from 'react-icons/io5';
+import { PiPencilCircleDuotone } from 'react-icons/pi';
 
 const links = [
     { name: 'Home', path: '/', },
@@ -35,6 +38,48 @@ const Nav = () => {
                     <UserButton />
                 </SignedIn>
             </div>
+            <Menu>
+                <MenuButton className="inline-flex items-center gap-2 rounded-md bg-gray-800 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
+                    Options
+                    <IoChevronDownCircleOutline className="size-4 fill-white/60" />
+                </MenuButton>
+
+                <MenuItems
+                    transition
+                    anchor="bottom"
+                    className="w-52 rounded-xl border border-white/5 bg-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+                >
+                    <MenuItem>
+                        <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
+                            <PiPencilCircleDuotone className="size-4 fill-white/30" />
+                            Edit
+                            {/* <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘E</kbd> */}
+                        </button>
+                    </MenuItem>
+                    <MenuItem>
+                        <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
+                            {/* <Square2StackIcon className="size-4 fill-white/30" /> */}
+                            Duplicate
+                            {/* <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd> */}
+                        </button>
+                    </MenuItem>
+                    <div className="my-1 h-px bg-white/5" />
+                    <MenuItem>
+                        <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
+                            {/* <ArchiveBoxXMarkIcon className="size-4 fill-white/30" /> */}
+                            Archive
+                            {/* <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘A</kbd> */}
+                        </button>
+                    </MenuItem>
+                    <MenuItem>
+                        <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
+                            {/* <TrashIcon className="size-4 fill-white/30" /> */}
+                            Delete
+                            {/* <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd> */}
+                        </button>
+                    </MenuItem>
+                </MenuItems>
+            </Menu>
             <ThemeToggle />
         </nav>
     );
