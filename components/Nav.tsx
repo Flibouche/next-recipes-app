554fac3be59f29572b86fa210da09b154b673410
Flibouche <kevin.pfiffer2@gmail.com>
2024-12-02 22:31:05 +0100

    📦 UPDATE: Enhance Nav component with admin dashboard links; update user role handling and improve dropdown menu structure.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -18,6 +18,7 @@ import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 // Icons
 import { IoChevronDownCircleOutline, IoHome, IoFastFood } from 'react-icons/io5';
 import { PiArticleNyTimesFill } from "react-icons/pi";
+import { MdAdminPanelSettings } from "react-icons/md";
 import { CiLogin } from "react-icons/ci";
 import { FaUser } from "react-icons/fa";
 
@@ -29,17 +30,18 @@ const links = [
 ];
 
 const adminLinks = [
-    { name: 'Admin', path: '/admin', icon: <IoHome className='size-4' /> },
+    { name: 'Admin dashboard', path: '/admin', icon: <MdAdminPanelSettings className='size-4' /> },
 ]
 
 const Nav = () => {
     const pathname: string = usePathname();
 
     const { session } = useSession();
-    const isAdmin = checkUserRoleByMetadata(session);
+    const userRole = checkUserRoleByMetadata(session);
 
     return (
         <nav className="flex items-center gap-8" aria-label='Desktop navigation'>
+            {/* Links */}
             {links.map((link, index) => (
                 <Link
                     href={link.path}
@@ -53,62 +55,66 @@ const Nav = () => {
                     </div>
                 </Link>
             ))}
-            {session ? (
+
+            {/* Admin dropdown */}
+            {session && userRole === 'admin' && (
                 <Menu>
-                    <MenuButton className="inline-flex gap-2 rounded-md pb-1 duration-300 ease-in-out hover:text-primary-800">
+                    <MenuButton className="relative inline-flex gap-2 rounded-md pb-1 duration-300 ease-in-out hover:text-primary-800">
                         <IoChevronDownCircleOutline className="size-5" />
                     </MenuButton>
 
                     <MenuItems
                         transition
                         anchor="bottom"
-                        className="w-40 rounded-xl border border-white/5 bg-black/75 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+                        className="!absolute !top-14 w-44 rounded-xl border border-white/5 bg-black/75 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
                     >
-                        {adminLinks.map((link, index) => (
-                            <Link
-                                href={link.path}
-                                key={index}
-                            >
-                                <MenuItem>
-                                    <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
-                                        {link.icon}
-                                        {link.name}
-                                    </button>
-                                </MenuItem>
-                            </Link>
-                        ))}
+                        <div>
+                            <MenuItem>
+                                <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
+                                    Archive
+                                </button>
+                            </MenuItem>
+                            <MenuItem>
+                                <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
+                                    Delete
+                                </button>
+                            </MenuItem>
+                        </div>
                         <div className="my-1 h-px bg-white/5" />
-                        {isAdmin && (
+                        {userRole === 'admin' && (
                             <div>
-                                <MenuItem>
-                                    <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
-                                        {/* <ArchiveBoxXMarkIcon className="size-4 fill-white/30" /> */}
-                                        Archive
-                                        {/* <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘A</kbd> */}
-                                    </button>
-                                </MenuItem>
-                                <MenuItem>
-                                    <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
-                                        {/* <TrashIcon className="size-4 fill-white/30" /> */}
-                                        Delete
-                                        {/* <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd> */}
-                                    </button>
-                                </MenuItem>
+                                {adminLinks.map((link, index) => (
+                                    <Link
+                                        href={link.path}
+                                        key={index}
+                                    >
+                                        <MenuItem>
+                                            <button className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-white/10">
+                                                {link.icon}
+                                                {link.name}
+                                            </button>
+                                        </MenuItem>
+                                    </Link>
+                                ))}
                             </div>
                         )}
                     </MenuItems>
-                    <SignedIn>
-                        <UserButton>
-                            <UserButton.MenuItems>
-                                <UserButton.Action
-                                    label="My Profile"
-                                    labelIcon={<FaUser />}
-                                    onClick={() => window.location.href = '/user-profile'}
-                                />
-                            </UserButton.MenuItems>
-                        </UserButton>
-                    </SignedIn>
                 </Menu>
+            )}
+
+            {/* User Button */}
+            {session ? (
+                <SignedIn>
+                    <UserButton>
+                        <UserButton.MenuItems>
+                            <UserButton.Action
+                                label="My Profile"
+                                labelIcon={<FaUser />}
+                                onClick={() => window.location.href = '/user-profile'}
+                            />
+                        </UserButton.MenuItems>
+                    </UserButton>
+                </SignedIn>
             ) : (
                 <>
                     <SignedOut>
@@ -123,8 +129,10 @@ const Nav = () => {
                     </SignedOut>
                 </>
             )}
+
+            {/* Dark / Light mode */}
             <ThemeToggle />
-        </nav>
+        </nav >
     );
 };
 
